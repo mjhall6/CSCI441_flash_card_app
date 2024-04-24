20d09785bd83fcf2fc0a93262c06763d9b79b0a6
Joseph Tracy <jdtracy@mail.fhsu.edu>
2024-04-24 12:34:20 -0500

    delete search func

--- routes/flashcardRoutes.js
+++ routes/flashcardRoutes.js
@@ -3,7 +3,7 @@ const flashCardController = require('../controllers/flashCardController');
 const flashCardRouter = express.Router();
 
 flashCardRouter.get('/', flashCardController.getFlashcards);
-flashCardRouter.get('/search', flashCardController.searchFlashcards);
+
 // insertFlashcards 
 // flashCardRouter.post('/add-cards', flashCardController.insertFlashcards);
 
