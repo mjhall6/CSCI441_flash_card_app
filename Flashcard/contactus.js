c31e5b7dbf2545ca23ac8bf6422c8d7f1ca44850
ahmedjaff90 <74609967+ahmedjaff90@users.noreply.github.com>
2024-04-24 11:14:46 -0500

    Delete Flashcard directory

--- Flashcard/contactus.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-
-    //display conformation message
-    document.getElementById('confirmationMessage').style.display = 'block';
-
-    //reset fields
-    document.getElementById('contactForm').reset();
-});
